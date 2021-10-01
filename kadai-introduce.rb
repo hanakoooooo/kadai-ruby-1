1e83b4ccd2684ed4ab3aaefd875ba7422fd88a7e
hanako.yamada <h.yamada20210929@gmail.com>
2021-10-01 12:18:25 +0000

    modified2 kadai-introduce.rb

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,6 +2,4 @@ lastname = '長南'
 firstname = 'ひかる'
 age = 27
 
-introduce = [lastname,firstname,'です。',age.to_s,'歳です。']
-
-puts introduce[0..4] .join
+puts lastname + firstname + 'です。' + age.to_s + '歳です。'
